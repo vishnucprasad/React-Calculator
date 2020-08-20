d342a6412065f1762442f046380e911a779fc940
Vishnu-C-Prasad <mr.vishnucp001@outlook.com>
2020-08-20 11:20:07 +0530

    Updates

---
+++ precache-manifest.d9a6768cbf318e671a6c25033d00077c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2262fc6ebb296aecf4e3c389ba798c2e",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "6e371fe2716ac09f5d5d",
+    "url": "/React-Calculator/static/css/main.00469d93.chunk.css"
+  },
+  {
+    "revision": "fc81ca433bef21005f93",
+    "url": "/React-Calculator/static/js/2.49490e8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Calculator/static/js/2.49490e8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e371fe2716ac09f5d5d",
+    "url": "/React-Calculator/static/js/main.7fbe7060.chunk.js"
+  },
+  {
+    "revision": "31f1b382c927167c40c0",
+    "url": "/React-Calculator/static/js/runtime-main.b6e8a5a6.js"
+  },
+  {
+    "revision": "a0e30e032b793717b00514b38236dc8a",
+    "url": "/React-Calculator/static/media/logo.a0e30e03.png"
+  }
+]);
